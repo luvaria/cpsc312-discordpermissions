7bc2490fcc3c17712db04ad95ee4f06dd1bdc984
Rebecca Li <luvaria@students.cs.ubc.ca>
2019-11-17 14:37:50 -0800

    Fix bugs; query produces 1 permissions integer

--- permissions.pl
+++ permissions.pl
@@ -12,7 +12,7 @@ flag("NONE", 0x00000000).
 base_permissions(Member, Guild, Roles, Permissions) :-
 	guild_owner(Member, Guild, P1),
 	flag("NONE", P2),
-	guild_roles(Roles, P2, P3),
+	guild_roles(Member, Guild, Roles, P2, P3),
 	guild_admin(P3, P4),
 	Permissions is P1 \/ P4.
 
@@ -27,13 +27,26 @@ guild_owner(Member, Guild, Permissions) :-
 	Member.user.id \= Guild.owner_id,
 	flag("NONE", Permissions).
 
-% given a list of roles, produces combined permissions integer
-	% includes @everyone, which has role.id == guild.id
-% guild_roles([Role], PermsIn, PermsOut).
-guild_roles([], P, P).
-guild_roles([H|T], P, R) :-
+% given member and list of roles in guild,
+% produces combined permissions integer
+% guild_roles(Member, Guild, [Role], PermsIn, PermsOut).
+guild_roles(_, _, [], P, P).
+% @everyone has role.id = guild.id
+guild_roles(M, G, [H|T], P, R) :-
+	H.id = G.id,
 	P1 is P \/ H.permissions,
-	guild_roles(T, P1, R).
+	guild_roles(M, G, T, P1, R).
+% member has role
+guild_roles(M, G, [H|T], P, R) :-
+	memberchk(H.id, M.roles),
+	P1 is P \/ H.permissions,
+	guild_roles(M, G, T, P1, R).
+% member does not have role
+guild_roles(M, G, [H|T], P, R) :-
+	H.id \= G.id,
+	\+ memberchk(H.id, M.roles),
+	guild_roles(M, G, T, P, R).
+
 
 % PermsIn has administrator
 % guild_admin(PermsIn, PermsOut).
@@ -56,7 +69,9 @@ overwrite_permissions(_, _, Base, Permissions) :-
 overwrite_permissions(Member, Channel, Base, Permissions) :-
 	\+ has_permission(Base, "ADMINISTRATOR"),
 	channel_everyone(Channel, Base, P1),
-	channel_roles(Channel.permission_overwrites, Channel.guild_id, P1, P2),
+	flag("NONE", N),
+	channel_roles(Channel.permission_overwrites, Channel.guild_id, N, N, Deny, Allow),
+	P2 is P1 /\ \Deny \/ Allow, 
 	channel_member(Channel.permission_overwrites, Member, P2, Permissions).
 
 % helpers for overwrite_permissions:
@@ -68,35 +83,45 @@ channel_everyone(Channel, Base, Permissions) :-
 	Deny is Base /\ \Everyone.deny,
 	Permissions is Deny \/ Everyone.allow.
 
-% given a list of overwrites, channel, and initial permissions,
-% produces combined permissions integer for roles
-% channel_roles([Overwrite], GuildID, Base, Permissions).
-channel_roles([], _, P, P).
-% ignore member overwrites
-channel_roles([H|T], GID, P, R) :-
-	H.type = "member",
-	channel_roles(T, GID, P, R).
+% given a list of overwrites and a channel,
+% produces combined permissions integers for roles
+% channel_roles([Overwrite], GuildID, DenyIn, AllowIn, Deny, Allow).
+channel_roles([], _, D, A, D, A).
+% ignore non-role overwrites
+channel_roles([H|T], GID, D1, A1, D2, A2) :-
+	H.type \= "role",
+	channel_roles(T, GID, D1, A1, D2, A2).
 % ignore @everyone overwrites
-channel_roles([H|T], GID, P, R) :-
+channel_roles([H|T], GID, D1, A1, D2, A2) :-
 	H.id = GID,
-	channel_roles(T, GID, P, R).
+	channel_roles(T, GID, D1, A1, D2, A2).
 % role overwrites
-channel_roles([H|T], GID, P, R) :-
-	H.type \= "member",
+channel_roles([H|T], GID, D1, A1, D2, A2) :-
+	H.type = "role",
 	H.id \= GID,
-	D is P /\ \H.deny,
-	P1 is D \/ H.allow,
-	channel_roles(T, GID, P1, R).
+	D3 is D1 \/ H.deny,
+	A3 is A1 \/ H.allow,
+	channel_roles(T, GID, D3, A3, D2, A2).
 
 % given a list of overwrites, member, and initial permissions,
 % produces combined permissions integer for members
 % channel_member([Overwrite], Member, PermsIn, PermsOut).
-channel_member(Overwrites, Member, PermsIn, PermsOut) :-
-	member(MemberOverwrite, Overwrites),
-	MemberOverwrite.type = "member",
-	MemberOverwrite.id = Member.user.id,
-	Deny is PermsIn /\ \MemberOverwrite.deny,
-	PermsOut is Deny \/ MemberOverwrite.allow.
+channel_member([], _, P, P).
+% ignore non-member overwrites
+channel_member([H|T], M, PIn, POut) :-
+	H.type \= "member",
+	channel_member(T, M, PIn, POut).
+% overwrites doesn't match member id
+channel_member([H|T], M, PIn, POut) :-
+	H.type = "member",
+	H.id \= M.user.id,
+	channel_member(T, M, PIn, POut).
+% overwrite matches member id
+channel_member([H|_], M, PIn, POut) :-
+	H.type = "member",
+	H.id = M.user.id,
+	Deny is PIn /\ \H.deny,
+	POut is Deny \/ H.allow.
 
 
 % given a member of a guild and a channel in the guild,
