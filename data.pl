d4464c343d0c111c7b920b518c9fece34890e689
Rebecca Li <luvaria@students.cs.ubc.ca>
2019-11-16 23:36:45 -0800

    Add member and channel endpoints; rename to data.pl

--- data.pl
+++ data.pl
@@ -11,6 +11,10 @@
 guild_URL(GuildID, GuildURL) :-
 	string_concat("https://discordapp.com/api/guilds/", GuildID, GuildURL).
 
+% forms URL to access channel by ID through Discord API
+channel_URL(ChannelID, ChannelURL) :-
+	string_concat("https://discordapp.com/api/channels/", ChannelID, ChannelURL).
+
 % various endpoints for guilds can be found here: 
 % https://discordapp.com/developers/docs/resources/guild
 
@@ -24,10 +28,16 @@ channels_URL(GuildID, ChannelsURL) :-
 	guild_URL(GuildID, GuildURL),
 	string_concat(GuildURL, "/channels", ChannelsURL).
 
-% forms URL to access to guild members
+% forms URL to access guild members
 members_URL(GuildID, MembersURL) :-
 	guild_URL(GuildID, GuildURL),
 	string_concat(GuildURL, "/members", MembersURL).
+
+% forms URL to access individual guild member by user ID
+member_URL(GuildID, UserID, MemberURL) :-
+	members_URL(GuildID, MembersURL),
+	string_concat(MembersURL, "/", URL),
+	string_concat(URL, UserID, MemberURL).
 
 % forms URL to access guild bans
 bans_URL(GuildID, BansURL) :-
@@ -44,18 +54,26 @@ invites_URL(GuildID, InvitesURL) :-
 	guild_URL(GuildID, GuildURL),
 	string_concat(GuildURL, "/invites", InvitesURL).
 
-% gets data as dict given URL, tagging by id
+
+% gets data as dict given URL
 get_data(URL, Dict) :-
 	bot_token(Token),
 	catch(http_open(URL, Stream, [request_header("Authorization"=Token)]),
 		error(Err, _),
 		(format("Error while retrieving data:\n    ~w\nDoes bot have access to guild / Is guild ID correct?\n", [Err]),
 		  fail)),
-	json_read_dict(Stream, Dict, [tag(id)]).
+	json_read_dict(Stream, Dict).
 
-% gets roles as dict given guild ID
+
 % guilds are synonymous to servers - collection of users and channels
 % e.g. ?- get_roles("6317111103203792896",X).
+
+% gets guild as dict given guild ID
+get_guild(GuildID, Dict) :-
+	guild_URL(GuildID, URL),
+	get_data(URL, Dict).
+
+% gets roles as dict given guild ID
 get_roles(GuildID, Dict) :-
 	roles_URL(GuildID, URL),
 	get_data(URL, Dict).
@@ -65,9 +83,19 @@ get_channels(GuildID, Dict) :-
 	channels_URL(GuildID, URL),
 	get_data(URL, Dict).
 
+% gets individual channel as dict given channel ID
+get_channel(ChannelID, Dict) :-
+	channel_URL(ChannelID, URL),
+	get_data(URL, Dict).
+
 % gets members as dict given guild ID
 get_members(GuildID, Dict) :-
 	members_URL(GuildID, URL),
+	get_data(URL, Dict).
+
+% gets individual member as dict given guild ID and user ID
+get_member(GuildID, UserID, Dict) :-
+	member_URL(GuildID, UserID, URL),
 	get_data(URL, Dict).
 
 % gets bans as dict given guild ID
