de65102d1d1487c76007ccb47a8738316036ad53
Rebecca Li <luvaria@students.cs.ubc.ca>
2019-11-24 15:34:21 -0800

    Refactor permissions to use IDs and to separate getting guild-wide and channel-specific permissions integers; channel_everyone works when there is no @everyone overwrite (occurs when none have ever been set)

--- permissions.pl
+++ permissions.pl
@@ -2,16 +2,19 @@
 
 :- consult(flags).
 
+
 % all permissions granted
 flag("ALL", 0x7fffffff).
 % no permissions granted
 flag("NONE", 0x00000000).
 
+
 % computes a member's base permissions integer
 % given member and guild
-base_permissions(Member, Guild, Roles, Permissions) :-
+base_permissions(Member, Guild, Permissions) :-
 	guild_owner(Member, Guild, P1),
 	flag("NONE", P2),
+	Roles = Guild.roles,
 	guild_roles(Member, Guild, Roles, P2, P3),
 	guild_admin(P3, P4),
 	Permissions is P1 \/ P4.
@@ -68,7 +71,7 @@ overwrite_permissions(_, _, Base, Permissions) :-
 % base is not administrator
 overwrite_permissions(Member, Channel, Base, Permissions) :-
 	\+ has_permission(Base, "ADMINISTRATOR"),
-	channel_everyone(Channel, Base, P1),
+	channel_everyone(Channel.permission_overwrites, Channel.guild_id, Base, P1),
 	flag("NONE", N),
 	channel_roles(Channel.permission_overwrites, Channel.guild_id, N, N, Deny, Allow),
 	P2 is P1 /\ \Deny \/ Allow, 
@@ -77,11 +80,16 @@ overwrite_permissions(Member, Channel, Base, Permissions) :-
 % helpers for overwrite_permissions:
 
 % given a channel, applies @everyone overwrites
-channel_everyone(Channel, Base, Permissions) :-
-	member(Everyone, Channel.permission_overwrites),
-	Everyone.id = Channel.guild_id, % can this throw an error? @everyone overwrite may not exist
-	Deny is Base /\ \Everyone.deny,
-	Permissions is Deny \/ Everyone.allow.
+% channel_everyone([Overwrite], GuildID, Base, Permissions).
+channel_everyone([], _, P, P).
+channel_everyone([H|T], GID, B, P) :-
+	H.id \= GID,
+	channel_everyone(T, GID, B, P).
+% @everyone overwrite found (assume @everyone only shows up once in overwrites)
+channel_everyone([H|_], GID, B, P) :-
+	H.id = GID,
+	D is B /\ \H.deny,
+	P is D \/ H.allow.
 
 % given a list of overwrites and a channel,
 % produces combined permissions integers for roles
@@ -124,11 +132,26 @@ channel_member([H|_], M, PIn, POut) :-
 	POut is Deny \/ H.allow.
 
 
-% given a member of a guild and a channel in the guild,
-% produces the member's permissions considering overwrites
-	% Member, Guild, Channel as dicts
+% given the IDs of a member of a guild and the guild itself,
+% produces the member's base (guild-wide) permissions
 	% Permissions as integer 0x########
-% TODO: alternatively, get Guild and Roles based on Channel (from guild_id) rather than passing args
-permissions(Member, Guild, Channel, Roles, Permissions) :-
-	base_permissions(Member, Guild, Roles, Base),
+% try
+	% guild_permissions("645375281124737025", "631724803203792896", P).
+guild_permissions(UserID, GuildID, Permissions) :-
+	get_member(GuildID, UserID, Member),
+	get_guild(GuildID, Guild),
+	base_permissions(Member, Guild, Permissions).
+	
+
+% given the IDs of a member of a guild and a channel in the guild,
+% produces the member's permissions considering overwrites
+	% Permissions as integer 0x########
+% try
+	% channel_permissions("645375281124737025", "645434356680097812", P).
+channel_permissions(UserID, ChannelID, Permissions) :-
+	get_channel(ChannelID, Channel),
+	GuildID = Channel.guild_id,
+	get_member(GuildID, UserID, Member),
+	get_guild(GuildID, Guild),
+	base_permissions(Member, Guild, Base),
 	overwrite_permissions(Member, Channel, Base, Permissions).
